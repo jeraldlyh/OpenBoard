e2aebd8e0f32f2ff4543902e82cad9046ec52d4f
Felice <felicepng2010@gmail.com>
2021-07-08 17:51:02 +0800

    WIP: Add page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import DoughnutChart from "../components/charts/doughnutChart"
 import LineChart from "../components/charts/lineChart"
 import MapChart from "../components/charts/mapChart"
 import PieChart from "../components/charts/pieChart"
+import { useRouter } from "next/router"
 
 export default function Home() {
+    const router = useRouter()
+
     return (
         <div className="bg-black h-full w-screen pt-8 pl-8 grid grid-cols-2">
             <BarChart name="Sample Bar" />
---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="bg-black h-full text-white w-screen pt-8 pl-8 grid grid-cols-2">
+            Test
+        </div>
+    )
+}
