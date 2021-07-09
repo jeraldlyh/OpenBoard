39aa7d9622d2fb197bcbae5f9dc30b70ec5e1307
Jerald <jeraldssw@gmail.com>
2021-07-09 13:30:48 +0800

    Create POST endpoint for slug

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,27 +1,52 @@
 import User from "../../../models/user"
 import connectDB from "../../../middleware/mongodb"
 import type { NextApiRequest, NextApiResponse } from "next"
-import mongoose from "mongoose"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { query: { id }, method } = req
+    const { query: { id }, method, body } = req
 
     switch (method) {
         case "GET":
             try {
-                const users = await User.find({
-                    "root_id": {
-                        $in: [mongoose.Types.ObjectId(id.toString())]
-                    }
-                })
+                const user = await User.findById(id)
 
-                if (!users) {
+                if (!user) {
                     return res.status(400).send("No user data found")
                 }
-                return res.status(200).send(users)
+                return res.status(200).send(user)
             } catch (error) {
                 return res.status(500).send(error.message)
             }
+            break
+
+        case "PUT":
+            try {
+                const user = await User.findByIdAndUpdate(id, body, {
+                    new: true,                  // Returns updated document
+                    runValidators: true         // Validate request body against schema
+                })
+
+                if (!user) {
+                    return res.status(400).send("No such user exists")
+                }
+                return res.status(200).send(user)
+            } catch (error) {
+                return res.status(500).send(error.message)
+            }
+            break
+
+        case "DELETE":
+            try {
+                const deletedUser = await User.deleteOne({ _id: id })
+                
+                if (!deletedUser) {
+                    return res.status(400).send("No such user exists")
+                }
+                return res.status(204).send(deletedUser)
+            } catch (error) {
+                return res.status(500).send(error.message)
+            }
+            break
     }
 }
 
