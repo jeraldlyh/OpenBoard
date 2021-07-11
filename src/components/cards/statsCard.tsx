60e67d7da2e529747efc5ed34f2303aace8218b3
Felice <felicepng2010@gmail.com>
2021-07-11 19:35:02 +0800

    Edit padding

--- src/components/cards/statsCard.tsx
+++ src/components/cards/statsCard.tsx
@@ -3,8 +3,8 @@ import { ImArrowUpRight2, ImArrowDownLeft2 } from "react-icons/im"
 
 function StatsCard(props: any) {
     return (
-        <div className="border border-th-outline bg-th-background-secondary rounded-2xl px-8 pt-6 pb-4 shadow-th-shadow flex justify-between items-center">
-            <div className="px-3 text-center relative z-10">
+        <div className="border border-th-outline bg-th-background-secondary rounded-2xl px-10 pt-6 pb-4 shadow-th-shadow flex justify-between items-center">
+            <div className="pr-3 text-center relative z-10">
                 <div className="text-left text-sm uppercase text-th-text">{props.name}</div>
                 <div className="flex items-center justify-between">
                     <div className="text-3xl text-th-text font-semibold my-3">{props.stats}</div>
