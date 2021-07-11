352d4338e5a50dddf74194b4d6668580667ab620
Felice <felicepng2010@gmail.com>
2021-07-11 19:38:27 +0800

    Edit padding #2

--- src/components/cards/statsCard.tsx
+++ src/components/cards/statsCard.tsx
@@ -3,7 +3,7 @@ import { ImArrowUpRight2, ImArrowDownLeft2 } from "react-icons/im"
 
 function StatsCard(props: any) {
     return (
-        <div className="border border-th-outline bg-th-background-secondary rounded-2xl px-10 pt-6 pb-4 shadow-th-shadow flex justify-between items-center">
+        <div className="border border-th-outline bg-th-background-secondary rounded-2xl px-10 pt-6 pb-3 shadow-th-shadow flex justify-between items-center">
             <div className="pr-3 text-center relative z-10">
                 <div className="text-left text-sm uppercase text-th-text">{props.name}</div>
                 <div className="flex items-center justify-between">
@@ -18,11 +18,11 @@ function StatsCard(props: any) {
             </div>
             {
                 props.change[0] === "-" ?
-                <div className="-mt-2 rounded-xl w-10 h-10 bg-redtrans flex justify-center items-center">
+                <div className="-mt-3 rounded-xl w-10 h-10 bg-redtrans flex justify-center items-center">
                     <ImArrowDownLeft2 className="w-5 h-5 text-red-500"/>
                 </div>
                 :
-                <div className="-mt-2 rounded-xl w-10 h-10 bg-greentrans flex justify-center items-center">
+                <div className="-mt-3 rounded-xl w-10 h-10 bg-greentrans flex justify-center items-center">
                     <ImArrowUpRight2 className="w-5 h-5 text-green-500"/>
                 </div>
             }
